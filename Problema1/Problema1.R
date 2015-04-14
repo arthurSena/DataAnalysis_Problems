7ea2767c5ce72fb5213e9880b232b275f3d2082c
arthurSena <arthur.senaufcg@gmail.com>
2015-04-14 13:39:25 -0300

    Finishing part 2 from problem one

--- Problema1/Problema1.R
+++ Problema1/Problema1.R
@@ -1,17 +1,11 @@
 salariosTI <- read.csv("salarios-ti-refinado.csv")
 salariosTI <- salariosTI[-1,]
 
-#Os dados que eu considero incosistentes
-temp <- salariosTI$Pos.Graduacao.ou.Certificacao == TRUE
-temp <- salariosTI[temp,]
-dados_inconsistentes <- temp[temp$Formaca == "Nao informou",]
-nrow(dados_inconsistentes)/nrow(salariosTI)
-
 #Melhor e estado e regiao para se trabalhar 
 estados <- levels(salariosTI$UF)
 estados_salario <- sapply(estados,function(estado){
   salario_estado <- salariosTI[salariosTI$UF == estado,]
-  mean(salario_estado$Salario.Bruto)
+  median(salario_estado$Salario.Bruto)
 }) 
 
 estados_salario <- as.data.frame(estados_salario)
@@ -23,7 +17,7 @@ rownames(estados_salario) <- NULL
 regioes <- levels(salariosTI$Regiao)
 regioes_salarios <- sapply(regioes, function(regiao){
    salario_regiao <- salariosTI[salariosTI$Regiao == regiao,]
-   mean(salario_regiao$Salario.Bruto)
+   median(salario_regiao$Salario.Bruto)
  })
 
 regioes_salarios <- as.data.frame(regioes_salarios)
