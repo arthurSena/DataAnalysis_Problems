ad9e69824593439bd0d0a6ce56f82fcd4df609ae
arthurSena <arthur.senaufcg@gmail.com>
2016-03-02 01:08:10 -0300

    Problem2 - checkpoint 1

--- DataCourse2/Problem2/mapper.py
+++ DataCourse2/Problem2/mapper.py
@@ -4,5 +4,5 @@ import sys
 
 for line in sys.stdin:
 	if len(line) > 1:
-		cod_linha = line.strip().split(',')[0].split(":")[1]
+		cod_linha = line.strip().split(',')[1].split(":")[1]
 		print "%s\t%s" % (cod_linha,1)
