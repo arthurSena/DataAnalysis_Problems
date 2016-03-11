0e45b481e99f75c0ef646615392dcb390173eb55
arthurSena <arthur.senaufcg@gmail.com>
2016-03-11 01:25:41 -0300

    Problem 2 -checkpoint 2 Solved

--- DataCourse2/Problem2/mapper5.py
+++ DataCourse2/Problem2/mapper5.py
@@ -2,8 +2,15 @@
 
 import sys
 
+# for line in sys.stdin:
+# 	if len(line) > 1:
+# 		numero_cartao = line.strip().split(',')[3].split(":")[1]
+# 		cod_veic = line.strip().split(',')[2].split(":")[1]
+# 		print "%s\t%s" % (numero_cartao,cod_veic)
+
 for line in sys.stdin:
 	if len(line) > 1:
-		numero_cartao = line.strip().split(',')[3].split(":")[1]
+		tempo = line.strip().split(',')[4].split(":")[1].replace("\"","") + ":" +line.strip().split(',')[4].split(":")[2].replace("\"","")
 		cod_veic = line.strip().split(',')[2].split(":")[1]
-		print "%s\t%s" % (numero_cartao,cod_veic)
+		numero_cartao = line.strip().split(',')[3].split(":")[1]
+		print tempo + "-" + cod_veic + "\t" + numero_cartao
--- DataCourse2/Problem2/reducer5.py
+++ DataCourse2/Problem2/reducer5.py
@@ -2,28 +2,52 @@
 
 import sys
 
-def check_numCartao(mapa, codV, numC):
-	for k in mapa.keys():
-		if k != numC and codV in mapa[k]:
-			print "%s\t%s" % (numC,k)
+tempo_temp = ""
+tempo_autal = ""
+cartoes = []
 
-mapa = {}
-mapaFinal = {}
 
 for line in sys.stdin:
-	numero_cartao = line.split('\t')[0]
-	cod_veic = line.split('\t')[1].strip()
+	tempo = line.strip().split('\t')[0]
+	cartao = line.strip().split('\t')[1]
 
-	if not mapa:
-		mapa[numero_cartao] = [cod_veic]
+	if tempo_temp == "":
+		tempo_temp = tempo
+		tempo_autal = tempo
+		cartoes.append(cartao)
+	elif tempo_temp == tempo:
+		cartoes.append(cartao)
 	else:
-		if numero_cartao in mapa.keys():
-			if cod_veic not in mapa[numero_cartao]:
-				mapa[numero_cartao].append(cod_veic)
-		else:
-			mapa[numero_cartao] = [cod_veic]
-		# check_numCartao(mapa, cod_veic	, numero_cartao)
+		print "%s\t%s\t%s" % (tempo , "-".join(cartoes) , len(cartoes))
+ 		tempo_temp = tempo
+		tempo_autal = tempo
+		cartoes = []
+		cartoes.append(cartao)
+if tempo_autal == tempo_temp:
+	print "%s\t%s\t%s" % (tempo , "-".join(cartoes) , len(cartoes))
+
+# def check_numCartao(mapa, codV, numC):
+# 	for k in mapa.keys():
+# 		if k != numC and codV in mapa[k]:
+# 			print "%s\t%s" % (numC,k)
+
+# mapa = {}
+# mapaFinal = {}
+
+# for line in sys.stdin:
+# 	numero_cartao = line.split('\t')[0]
+# 	cod_veic = line.split('\t')[1].strip()
+
+# 	if not mapa:
+# 		mapa[numero_cartao] = [cod_veic]
+# 	else:
+# 		if numero_cartao in mapa.keys():
+# 			if cod_veic not in mapa[numero_cartao]:
+# 				mapa[numero_cartao].append(cod_veic)
+# 		else:
+# 			mapa[numero_cartao] = [cod_veic]
+# 		# check_numCartao(mapa, cod_veic	, numero_cartao)
 
 
-for k in mapa.keys():
-	print "%s\t%s" %(k , mapa[k])
+# for k in mapa.keys():
+# 	print "%s\t%s" %(k , mapa[k])
