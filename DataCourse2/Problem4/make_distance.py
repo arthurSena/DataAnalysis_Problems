457bcfc8970611707e4846f3e99a8eca0a806827
arthur <arthur.senaufcg@gmail.com>
2016-04-15 18:17:36 -0300

    checkpoint 2

--- DataCourse2/Problem4/make_distance.py
+++ DataCourse2/Problem4/make_distance.py
@@ -1,20 +1,50 @@
 #!/usr/bin/python
 import fileinput
 import math
+import os
+from os import listdir
 
 def dist(lat1,lon1, lat2, lon2):
 	return math.sqrt((float(lat1)-float(lat2))**2 + (float(lon1)-float(lon2))**2)
 
-i = 1
-temp_line = []
-for line in fileinput.input(): 
-		temp = line.strip().split("\t")
-		if i == 1:
-			temp.append(0.0)
-		else:
-			temp.append(temp_line[len(temp_line) - 1]+ dist(temp[1],temp[2],temp_line[1], temp_line[2]))
-			# x = str(temp[len(temp) - 1])
-			# temp[len(temp)-1] = 0
-		print '\t'.join(str(e) for e in temp)
-		i+=1
-		temp_line = temp
+
+path = "/home/arthur/allLines/doc2-20151022/"
+
+allLines = os.listdir("/home/arthur/allLines/doc2-20151022/")
+
+
+for arq_nome in allLines:
+	f = open(path + arq_nome,'r')
+	i = 1
+	temp_line = []
+	conteudo = ""
+	for line in f.readlines(): 
+			temp = line.strip().split("\t")
+			if i == 1:
+				temp.append(0.0)
+			else:
+				temp.append(temp_line[len(temp_line) - 1]+ dist(temp[0],temp[1],temp_line[0], temp_line[1]))
+				# x = str(temp[len(temp) - 1])
+				# temp[len(temp)-1] = 0
+			conteudo = conteudo + '\t'.join(str(e) for e in temp) + "\n"
+			i+=1
+			temp_line = temp
+	novo_arq = open(path + arq_nome + "-dist.csv" , "w+")
+	novo_arq.write(conteudo)
+
+
+
+
+# i = 1
+# temp_line = []
+# for line in fileinput.input(): 
+# 		temp = line.strip().split("\t")
+# 		if i == 1:
+# 			temp.append(0.0)
+# 		else:
+# 			temp.append(temp_line[len(temp_line) - 1]+ dist(temp[1],temp[2],temp_line[1], temp_line[2]))
+# 			# x = str(temp[len(temp) - 1])
+# 			# temp[len(temp)-1] = 0
+# 		print '\t'.join(str(e) for e in temp)
+# 		i+=1
+# 		temp_line = temp
--- DataCourse2/Problem4/make_time.py
+++ DataCourse2/Problem4/make_time.py
@@ -3,17 +3,52 @@ import fileinput
 import math
 import time
 import datetime
+import os
+from os import listdir
 
-i = 1
-temp_line = []
-for line in fileinput.input(): 
-		temp = line.strip().split("\t")
-		if i == 1:
-			temp.append(0.0)
-		else:
-			temp.append(temp_line[len(temp_line) - 1]+ dist(temp[1],temp[2],temp_line[1], temp_line[2]))
-			# x = str(temp[len(temp) - 1])
-			# temp[len(temp)-1] = 0
-		print '\t'.join(str(e) for e in temp)
-		i+=1
-		temp_line = temp
+
+
+path = "/home/arthur/allLines/doc2-20151025/"
+
+allLines = os.listdir("/home/arthur/allLines/doc2-20151025/")
+
+
+for arq_nome in allLines:
+	if "-dist.csv" in arq_nome:
+		f = open(path + arq_nome,'r')
+		i = 1
+		temp_line = []
+		conteudo = ""
+		for line in f.readlines(): 
+				temp = line.strip().split("\t")
+				if i == 1:
+					temp.append(0.0)
+				else:
+					t_stamp1 = time.mktime(datetime.datetime.strptime(temp_line[2], "%d\/%m\/%Y %H:%M:%S").timetuple())
+					t_stamp2 = time.mktime(datetime.datetime.strptime(temp[2], "%d\/%m\/%Y %H:%M:%S").timetuple())
+					resultado = float(temp_line[len(temp_line) - 1]) +  (t_stamp1 - t_stamp2)
+					temp.append(resultado)
+				temp_line = temp
+				temp = [temp[len(temp) - 2], temp[len(temp) - 1]] 
+				conteudo = conteudo + '\t'.join(str(e) for e in temp) + "\n"
+				i+=1
+				
+		novo_arq = open(path + arq_nome.replace("-dist.csv","").replace(".csv","") + "-completo.csv" , "w+")
+		novo_arq.write(conteudo.strip())
+		novo_arq.close()
+
+
+# i = 1
+# temp_line = []
+# for line in fileinput.input(): 
+# 		temp = line.strip().split("\t")
+# 		if i == 1:
+# 			temp.append(0.0)
+# 		else:
+# 			t_stamp1 = time.mktime(datetime.datetime.strptime(temp_line[2], "%d\/%m\/%Y %H:%M:%S").timetuple())
+# 			t_stamp2 = time.mktime(datetime.datetime.strptime(temp[2], "%d\/%m\/%Y %H:%M:%S").timetuple())
+
+# 			temp.append(temp_line[len(temp_line) - 1] +  (t_stamp1 - t_stamp2))
+# 		print '\t'.join(str(e) for e in temp)
+# 		i+=1
+# 		temp_line = temp
